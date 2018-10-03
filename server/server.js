437b5aa0a841d21a5122efe56415210955debf07
Aakash Babasaheb Koli <aakash.koli@mitel.com>
2018-10-03 20:03:46 +0530

    Event emitters for messages on both client and server side added

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Aakash',
+        text: 'Coding BootCamp test1'
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('user was disconnected');
     });
