5b4e0d76911eeafde66752626fe68a03bc7f54ef
Aakash Babasaheb Koli <aakash.koli@mitel.com>
2018-10-03 21:50:55 +0530

    Emit newMessage from createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Beliver',
-        text: 'Coding Bootcamp test2'
-    });
 });
 
 socket.on('disconnect', function() {
