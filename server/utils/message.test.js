1999aed53f28e5b786d2029dafcc9038f21b733c
Aakash Babasaheb Koli <aakash.koli@mitel.com>
2018-10-04 16:53:54 +0530

    Added geolocation support using geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,19 @@ describe('generateMessage', () => {
 
         expect(typeof message.createdAt).toBe('number');
         expect(message).toMatchObject({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = "Akoli";
+        var latitude = 12;
+        var longitude = 1;
+        var url = "https://www.google.com/maps?q=12,1"
+
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, url});
     });
 });
